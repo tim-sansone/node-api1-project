e2bcb8d6f8625072f7f74c2110ddba576a392e91
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-11 15:31:19 -0500

    all endpoints functional

--- api/server.js
+++ api/server.js
@@ -48,4 +48,22 @@ server.delete('/api/users/:id', (req, res) => {
         .catch(err => res.status(500).json({message: "The user could not be removed"}))
 })
 
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+    userModel.update(id, body)
+        .then(user => {
+            if(user == null){
+                res.status(404).json({message: "The user with the specified ID does not exist"})
+                return
+            }
+            if(body.name == null || body.bio == null){
+                res.status(400).json({message: "Please provide name and bio for the user"})
+                return
+            }
+            res.json(user)
+        })
+        .catch(err => res.status(500).json({message: "The user information could not be modified"}))
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
