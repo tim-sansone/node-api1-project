945db53ad4af92be2763a77edb62faa0d9f56a00
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-11 15:04:06 -0500

    post new user endpoint functional

--- api/server.js
+++ api/server.js
@@ -1,3 +1,21 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const server = express();
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const userModel = require('./users/model')
+
+server.post('/api/users', (req, res) => {
+    const { body } = req;
+    if(body.name == null || body.bio == null){
+        res.status(400).json({message: "Please provide name and bio for the user"})
+        return
+    }
+    userModel.insert(body).then(user => {
+        res.status(201).json(user)
+    })
+})
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+
+server.listen(port, () => {
+    console.log(`the server is now listening on port ${port}`)
+})
