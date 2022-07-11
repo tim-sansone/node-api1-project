486720176e1c2b90670d98e6fe17f8ee17f6c925
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-11 15:14:51 -0500

    get user by id endpoint functional

--- api/server.js
+++ api/server.js
@@ -22,5 +22,17 @@ server.get('/api/users', (req, res) => {
         .catch(err => res.status(500).json({message: "The users information could not be retrieved"}))
 })
 
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    userModel.findById(id)
+        .then(user => {
+            if(user == null){
+                res.status(404).json({message: "The user with the specified ID does not exist"})
+                return
+            }
+            res.json(user)
+        })
+        .catch(err => res.status(500).json({message: "The user information could not be retrieved"}))
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
